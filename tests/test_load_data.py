18370508f148092f6c84ba220c967d8976deeb0f
cyr-ius <cyr-ius@ipocus.net>
2023-01-14 20:31:18 +0100

    Add tests

---
+++ tests/test_load_data.py
@@ -0,0 +1,59 @@
+from datetime import datetime as dt
+from unittest.mock import patch
+
+import pytest
+
+import myelectricaldatapy
+from myelectricaldatapy import EnedisByPDL
+
+from .consts import DATASET
+
+PDL = "012345"
+TOKEN = "xxxxxxxxxxxxx"
+
+VALID = {
+    "valid": False,
+    "information": "Vous avez dépassé votre quota journalier (50)",
+    "consent_expiration_date": "2026-01-14T15:32:38",
+    "call_number": 65,
+    "quota_reached": True,
+    "quota_limit": 50,
+    "quota_reset_at": "2023-01-14T23:59:59.999999",
+    "last_call": "2023-01-07T13:58:24.532501",
+    "ban": False,
+}
+
+CONTRACT = {}
+TEMPO = "RED"
+
+
+@pytest.mark.asyncio
+async def test_load() -> None:
+    with patch.object(
+        EnedisByPDL, "async_get_details_consumption", return_value=DATASET
+    ), patch.object(
+        EnedisByPDL, "async_get_contract", return_value=CONTRACT
+    ), patch.object(
+        myelectricaldatapy.auth.EnedisAuth, "request", return_value={}
+    ), patch.object(
+        EnedisByPDL, "async_get_tempoday", return_value=TEMPO
+    ):
+        api = EnedisByPDL(token=TOKEN, pdl=PDL)
+        await api.async_load()
+        await api.async_get_max_power(dt.now(), dt.now())
+        await api.async_get_contract()
+        await api.async_get_tempoday()
+        await api.async_get_address()
+        await api.async_get_ecowatt()
+        await api.async_has_offpeak()
+        await api.async_check_offpeak(dt.now())
+        await api.async_get_identity()
+        await api.async_get_daily_consumption(dt.now())
+        await api.async_get_daily_production(dt.now())
+        await api.async_get_details_production(dt.now(), dt.now())
+        assert (
+            api.power_datas["consumption"]
+            == DATASET["meter_reading"]["interval_reading"]
+        )
+        await api.async_refresh()
+        await api.async_close()
