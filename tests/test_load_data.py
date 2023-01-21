baa8d0c9efe5bee1cf28144ae0ff40e477d42c6b
cyr-ius <cyr-ius@ipocus.net>
2023-01-21 13:29:40 +0100

    Fix tests

--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -7,7 +7,7 @@ from unittest.mock import patch
 import pytest
 
 import myelectricaldatapy
-from myelectricaldatapy import EnedisAnalytics, EnedisByPDL
+from myelectricaldatapy import EnedisAnalytics, EnedisByPDL, LimitReached
 
 from .consts import DATASET
 
@@ -15,6 +15,17 @@ PDL = "012345"
 TOKEN = "xxxxxxxxxxxxx"
 
 VALID = {
+    "valid": False,
+    "consent_expiration_date": "2026-01-14T15:32:38",
+    "call_number": 7,
+    "quota_reached": False,
+    "quota_limit": 50,
+    "quota_reset_at": "2023-01-14T23:59:59.999999",
+    "last_call": "2023-01-07T13:58:24.532501",
+    "ban": False,
+}
+
+INVALID = {
     "valid": False,
     "information": "Vous avez dépassé votre quota journalier (50)",
     "consent_expiration_date": "2026-01-14T15:32:38",
@@ -25,6 +36,7 @@ VALID = {
     "last_call": "2023-01-07T13:58:24.532501",
     "ban": False,
 }
+
 
 CONTRACT: dict[str, Any] = {}
 TEMPO = "RED"
@@ -37,58 +49,95 @@ async def test_load() -> None:
     ), patch.object(
         EnedisByPDL, "async_get_contract", return_value=CONTRACT
     ), patch.object(
-        myelectricaldatapy.auth.EnedisAuth, "request", return_value={}
+        EnedisByPDL, "async_valid_access", return_value=VALID
+    ), patch.object(
+        myelectricaldatapy.auth.EnedisAuth, "request", return_value=DATASET
     ), patch.object(
         EnedisByPDL, "async_get_tempoday", return_value=TEMPO
-    ), patch.object(
-        EnedisByPDL, "async_fetch_datas", return_value=DATASET
     ):
-        api = EnedisByPDL(token=TOKEN, pdl=PDL)
-        await api.async_load(
-            [
-                (
-                    "comsumption_load_curve",
-                    dt.strptime("2022-12-30", "%Y-%m-%d"),
-                    dt.strptime("2022-12-31", "%Y-%m-%d"),
-                )
-            ]
+        api = EnedisByPDL(token=TOKEN)
+        resultat = await api.async_fetch_datas(
+            service="comsumption_load_curve",
+            pdl=PDL,
+            start=dt.strptime("2022-12-30", "%Y-%m-%d"),
+            end=dt.strptime("2022-12-31", "%Y-%m-%d"),
         )
         assert (
-            api.power_datas["comsumption_load_curve"]
+            resultat["meter_reading"]["interval_reading"]
             == DATASET["meter_reading"]["interval_reading"]
         )
-
-        await api.async_get_max_power(dt.now(), dt.now())
-        await api.async_get_contract()
+        await api.async_get_max_power(PDL, dt.now(), dt.now())
+        await api.async_get_contract(PDL)
         await api.async_get_tempoday()
-        await api.async_get_address()
+        await api.async_get_address(PDL)
         await api.async_get_ecowatt()
-        await api.async_has_offpeak()
-        await api.async_check_offpeak(dt.now())
-        await api.async_get_identity()
-        await api.async_get_daily_consumption(dt.now(), dt.now())
-        await api.async_get_daily_production(dt.now(), dt.now())
-        await api.async_get_details_production(dt.now(), dt.now())
-        await api.async_refresh()
+        await api.async_has_offpeak(PDL)
+        await api.async_check_offpeak(PDL, dt.now())
+        await api.async_get_identity(PDL)
+        await api.async_get_daily_consumption(PDL, dt.now(), dt.now())
+        await api.async_get_daily_production(PDL, dt.now(), dt.now())
+        await api.async_get_details_production(PDL, dt.now(), dt.now())
         await api.async_close()
 
 
 @pytest.mark.asyncio
+async def test_limit_reached() -> None:
+    with patch.object(
+        EnedisByPDL, "async_get_contract", return_value=CONTRACT
+    ), patch.object(
+        EnedisByPDL, "async_valid_access", return_value=INVALID
+    ), pytest.raises(
+        LimitReached
+    ):
+        api = EnedisByPDL(token=TOKEN)
+        await api.async_fetch_datas(
+            service="comsumption_load_curve",
+            pdl=PDL,
+            start=dt.strptime("2022-12-30", "%Y-%m-%d"),
+            end=dt.strptime("2022-12-31", "%Y-%m-%d"),
+        )
+
+
+@pytest.mark.asyncio
 async def test_analytcis() -> None:
-    intervals = [(dt.strptime("08H00", "%HH%M"), dt.strptime("12H00", "%HH%M"))]
     dataset = DATASET["meter_reading"]["interval_reading"]
+    cumsum = 62.533
+    intervals = [
+        (dt.strptime("01H30", "%HH%M"), dt.strptime("08H00", "%HH%M")),
+        (dt.strptime("12H30", "%HH%M"), dt.strptime("14H00", "%HH%M")),
+    ]
     analytics = EnedisAnalytics(dataset)
     resultat = analytics.get_data_analytcis(
         convertKwh=True,
         convertUTC=True,
         intervals=intervals,
+        freq="D",
         groupby="date",
         summary=True,
+        cumsum=cumsum,
     )
+    print(resultat)
+    analytics = EnedisAnalytics(dataset)
+    cumsum = 744.86
+    intervals = [
+        (dt.strptime("00H00", "%HH%M"), dt.strptime("01H30", "%HH%M")),
+        (dt.strptime("08H00", "%HH%M"), dt.strptime("12H30", "%HH%M")),
+        (dt.strptime("14H00", "%HH%M"), dt.strptime("00H00", "%HH%M")),
+    ]
+    resultat = analytics.get_data_analytcis(
+        convertKwh=True,
+        convertUTC=True,
+        intervals=intervals,
+        freq="D",
+        groupby="date",
+        summary=True,
+        cumsum=cumsum,
+    )
+    print(resultat)
     offpeak = analytics.set_price(resultat, 0.1641, True)
-    assert offpeak[0]["value"] == 3.146
+    assert offpeak[0]["value"] == 29.63
     value = analytics.get_last_value(resultat, "date", "sum_value")
-    assert value == 51.765
+    assert value == 920.566
 
 
 @pytest.mark.asyncio
@@ -104,7 +153,7 @@ async def test_empty() -> None:
         summary=True,
     )
     offpeak = analytics.set_price(resultat, 0.1641, True)
-    assert offpeak == []
+    assert not offpeak
 
 
 @pytest.mark.asyncio
@@ -112,4 +161,4 @@ async def test_nodata() -> None:
     analytics = EnedisAnalytics([])
     resultat = analytics.get_data_analytcis()
     offpeak = analytics.set_price(resultat, 0.1641, True)
-    assert offpeak == []
+    assert not offpeak
