d54cc915d9bb6b6a7c09a6070961b33a5bca23ba
cyr-ius <cyr-ius@ipocus.net>
2023-03-26 15:38:25 +0200

    Fix tests ecowatt_day and tempo _day

--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -1,39 +1,56 @@
+"""Tests  Enedis api."""
 from __future__ import annotations
 
 from datetime import datetime as dt
 from unittest.mock import patch
 
 import pytest
+from freezegun import freeze_time
 
 import myelectricaldatapy
-from myelectricaldatapy import Enedis
+from myelectricaldatapy import Enedis, EnedisByPDL
 
 from .consts import ACCESS
-from .consts import DATASET_30 as DATASET
+from .consts import DATASET_30 as DS_30
 from .consts import ECOWATT, INVALID_ACCESS, INVALID_ECOWATT, TEMPO
 
 PDL = "012345"
 TOKEN = "xxxxxxxxxxxxx"
 
 
+@freeze_time("2023-01-23")
 @pytest.mark.asyncio
 async def test_ecowatt() -> None:
     """Test get ecowatt."""
+    api = Enedis(token=TOKEN)
     with patch.object(
         myelectricaldatapy.auth.EnedisAuth, "request", return_value=ECOWATT
     ):
-        api = Enedis(token=TOKEN)
         resultat = await api.async_get_ecowatt()
         assert resultat["2023-01-22"]["value"] == 1
 
     with patch.object(
         myelectricaldatapy.auth.EnedisAuth, "request", return_value=INVALID_ECOWATT
     ):
-        api = Enedis(token=TOKEN)
         resultat = await api.async_get_ecowatt()
         assert resultat.get("2023-01-22") is None
 
+    mypdl = EnedisByPDL(pdl=PDL, token=TOKEN)
+    dataset = DS_30
+    with patch.object(
+        myelectricaldatapy.auth.EnedisAuth, "request", return_value=dataset
+    ), patch.object(
+        myelectricaldatapy.Enedis, "async_get_ecowatt", return_value=ECOWATT
+    ):
+        mypdl.ecowatt_subscription(True)
+        await mypdl.async_update()
+        resultat = mypdl.consumption_stats
 
+    print(mypdl.ecowatt_day)
+    assert mypdl.ecowatt_day["message"] == "Pas d’alerte."
+
+
+@freeze_time("2023-3-3")
 @pytest.mark.asyncio
 async def test_tempoday() -> None:
     """Test get tempo day."""
@@ -41,8 +58,20 @@ async def test_tempoday() -> None:
         myelectricaldatapy.auth.EnedisAuth, "request", return_value=TEMPO
     ):
         api = Enedis(token=TOKEN)
-        resultat = await api.async_get_ecowatt()
+        resultat = await api.async_get_tempo()
         assert resultat["2023-3-1"] == "blue"
+
+    mypdl = EnedisByPDL(pdl=PDL, token=TOKEN)
+    dataset = DS_30
+    with patch.object(
+        myelectricaldatapy.auth.EnedisAuth, "request", return_value=dataset
+    ), patch.object(myelectricaldatapy.Enedis, "async_get_tempo", return_value=TEMPO):
+        mypdl.tempo_subscription(True)
+        await mypdl.async_update(svc_consumption="consumption_load_curve")
+        resultat = mypdl.consumption_stats
+
+    print(mypdl.tempo_day)
+    assert mypdl.tempo_day == "blue"
 
 
 @pytest.mark.asyncio
@@ -69,8 +98,9 @@ async def test_valid_access() -> None:
 @pytest.mark.asyncio
 async def test_fetch_data() -> None:
     """Test fetch data."""
+    dataset = DS_30
     with patch.object(
-        myelectricaldatapy.auth.EnedisAuth, "request", return_value=DATASET
+        myelectricaldatapy.auth.EnedisAuth, "request", return_value=dataset
     ):
         api = Enedis(token=TOKEN)
         resultat = await api.async_fetch_datas(
@@ -81,15 +111,16 @@ async def test_fetch_data() -> None:
         )
         assert (
             resultat["meter_reading"]["interval_reading"]
-            == DATASET["meter_reading"]["interval_reading"]  # noqa
+            == dataset["meter_reading"]["interval_reading"]  # noqa
         )
 
 
 @pytest.mark.asyncio
 async def test_load() -> None:
     """Test load object."""
+    dataset = DS_30
     with patch.object(
-        myelectricaldatapy.auth.EnedisAuth, "request", return_value=DATASET
+        myelectricaldatapy.auth.EnedisAuth, "request", return_value=dataset
     ):
         api = Enedis(token=TOKEN)
         await api.async_get_max_power(PDL, dt.now(), dt.now())
